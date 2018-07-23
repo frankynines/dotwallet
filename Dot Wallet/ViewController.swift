e1b9900164916d4b77c37be6ea2a34386c6b83ea
Franky Aguilar <franky@99centbrains.com>
2018-07-23 17:26:34 +0100

    Initial Commit

---
+++ Dot Wallet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dot Wallet
+//
+//  Created by Franky Aguilar on 7/23/18.
+//  Copyright © 2018 Ninth Industries. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

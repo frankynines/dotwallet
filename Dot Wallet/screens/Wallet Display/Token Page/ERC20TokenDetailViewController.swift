c9e3023ebf645525115c3b2f08fba113fe1b2443
Franky Aguilar <franky@99centbrains.com>
2018-08-10 08:49:09 +0100

    Tokens are now tappable

---
+++ Dot Wallet/screens/Wallet Display/Token Page/ERC20TokenDetailViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ERC20TokenDetailViewController.swift
+//  Dot Wallet
+//
+//  Created by Franky Aguilar on 8/9/18.
+//  Copyright © 2018 Ninth Industries. All rights reserved.
+//
+
+import Foundation
